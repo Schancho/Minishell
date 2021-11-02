cc4f45405936fb2cd47fe348b77bf2d6130540c5
za7adotexe <razaha@student.1337.ma>
2021-11-02 21:42:12 +0100

    add

---
+++ execution/builtins/cd.c
@@ -0,0 +1,8 @@
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
+cow;ulqhfi2hfp'qfhp2'ifh'2hfvwjln
